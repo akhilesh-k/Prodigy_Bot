08319f8f7e4db785e35621cd6175d6eabd511aa4
Akhilesh Kumar <akhilesh_k@outlook.com>
2017-04-01 14:45:26 +0530

    added gui

--- ultrasonic_client.py
+++ ultrasonic_client.py
@@ -1,3 +1,4 @@
+#added GUIT for the client
 import tkinter as tk
 import tkinter.font
 from gpiozero import LED
@@ -9,16 +10,16 @@ led=LED(21)
 def ledToggle():
   if led.is_lit:
     led.off()
-    ledButton["text"]="Turn on your LED"
+    ledButton["text"]="Turn Your LED on"
   else:
     led.on()
-    ledButton["text"]="Turn off your LED"
+    ledButton["text"]="Turn your LED off"
 def exitProgram():
     root.quit()
 
-ledButton=tk.Button(root, text="Turn on your LED", font=myFont, command=ledToggle, bg='bisque2', height=1, width=24)
+ledButton=tk.Button(root, text="Turn your LED on", font=myFont, command=ledToggle, bg='bisque2', height=1, width=24)
 ledButton.grid(row=0, sticky=tk.NSEW)
-exitButton=tk.Button(root, text='Goodbye', font=myFont, command=exitProgram, bg='cyan', height=1, width=6)
+exitButton=tk.Button(root, text='Close', font=myFont, command=exitProgram, bg='cyan', height=1, width=6)
 exitButton.grid(row=1, sticky=tk.E)
 
 tk.mainloop()
