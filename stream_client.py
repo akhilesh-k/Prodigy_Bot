0ecba55b6e4438c436297aa641baf4450c549f5b
Akhilesh Kumar <akhilesh_k@outlook.com>
2017-04-26 11:23:54 +0530

    added realtime stream client

--- stream_client.py
+++ stream_client.py
@@ -3,22 +3,23 @@ import socket
 import struct
 import time
 import picamera
+import sys
 
 
-# create socket and bind host
+# creating socket and host
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('192.168.1.100', 8000))
 connection = client_socket.makefile('wb')
 
 try:
     with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)      # resol
-        camera.framerate = 10               # frame rate
-        time.sleep(2)                       # delay for init
+        camera.resolution = (320, 240)     
+        camera.framerate = 10               
+        time.sleep(2)                       
         start = time.time()
         stream = io.BytesIO()
         
-        # send jpeg format video stream
+        # retriving in real time. format: jpeg
         for foo in camera.capture_continuous(stream, 'jpeg', use_video_port = True):
             connection.write(struct.pack('<L', stream.tell()))
             connection.flush()
@@ -34,5 +35,6 @@ finally:
     client_socket.close()
     
 """
+my note:
 PiCamera documentation: https://picamera.readthedocs.org/en/release-1.10/recipes2.html
 """
